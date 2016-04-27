c06e6f455253b78aecece8decea91038d042a08a
leetcode91 <leetcode91@gmail.com>
2016-04-27 16:13:52 -0400

    - Updated exception type - Minor refactorings

--- src/com/activities/api/ActivityException.java
+++ src/com/activities/api/ActivityException.java
@@ -1,32 +1,45 @@
 package com.activities.api;
 
+
 /**
  * A generic wrapper for exceptions thrown while executing activities.
  */
-public class ActivityException extends ServiceException {
-
+public class ActivityException extends Exception {
 	// Constants
 	
 	/**
 	 * The serialization version ID.
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 6389952399696008650L;
 	
 	// Constructors
-	
-	public ActivityException(String serviceType, String message, Throwable cause) {
-		super(serviceType, message, cause);
+
+	/**
+	 * Constructor for an exception that wraps a caught exception
+	 * 
+	 * @param activity The name of the activity that this error is reported by
+	 * @param message Description of the problem
+	 * @param cause Exception that caused the problem
+	 */
+	public ActivityException(String activity, String message, Throwable cause) {
+	    super("Activity " + activity + ": " + message, cause);
 	}
 	
-	public ActivityException(String serviceType, String message) {
-		super(serviceType, message);
-	}
-	
-	public ActivityException(String message, Throwable cause) {
-		super("ActivityService", message, cause);
+	/**
+	 * Constructor for an exception that was not caused by another one
+	 * 
+	 * @param activity The name of the activity that this error is reported by
+	 * @param message Description of the problem
+	 */
+	public ActivityException(String activity, String message) {
+	    this(activity, message, null);
 	}
 	
 	public ActivityException(String message) {
-		super("ActivityService", message);
+		this("Activity", message);
+	}
+
+	public ActivityException(String message, Throwable e) {
+		this("Activity", message, e);
 	}
 }
